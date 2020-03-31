d14dd48e8e91da037fc25cd0ed9f3a3b394c4ce6
Temple <el.temple@gmail.com>
2020-03-31 17:34:16 +0200

    Babel-loader used in webpack for bundling the app

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,20 @@ module.exports = {
   entry: './src/main.mjs',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'es6.bundle.js'
-  }  
+    filename: 'es5.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  } 
 };
